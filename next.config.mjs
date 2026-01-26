fe5e00b162c37555dd8dac6fb56953202cf2d672
chocosykr <sejalrju8@gmail.com>
2026-01-26 14:54:28 +0530

    prisma setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    // This forces Turbopack to treat Prisma as a Node.js-only package
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
 };
 
 export default nextConfig;
